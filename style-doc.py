7c7ea84ae46bb709bb042c71a0d83985b7539361
jedijackatack <jedijackattack@gmail.com>
2016-06-24 14:00:06 +0100

    Updated style doc
    It now has rules for using issues tracker and most of the rules for coding and imports. Everyone must read.

--- style-doc.py
+++ style-doc.py
@@ -1,11 +1,16 @@
+# Created 24/06/2016 #
+# Last edited 24/06/2016 #
+# Most recent changes made by: jedijackattack #
+
 '''
-------Basics-----
+------Basics for code-----
 
 Indent size must be 4
     like this
         and this.
         
 All functions must have docstrings
+
     def BasicTestFunction(x,y):
         """This is the docstring explain the function here if you don't it won't be merged"""
         print(x,y)
@@ -13,9 +18,10 @@ All functions must have docstrings
         
 All variables and functions that are not 1 letter tempary vars must start with capital letters and follow the C 
 variable guide. e.g. onetwothree would be OneTwoThree.
-    BasicValue = 4
-    def TheFunction(x,BasicValue):
-    """multiplies two numbers"""
+    
+    def TheFunction(x):
+        """multiplies two numbers"""
+        BasicValue = 4
         print(x*BasicValue)
         pass
 
@@ -23,11 +29,27 @@ All functions must end in pass, continue, break or return.
 
 Functions should do one task and one task only. If it can be split into 2 tasks split it. They can call other functions.
 
-A classes __init__ method must be the first function in the class file.
+A classes __init__ function must be the first function in the class file.
+
+We will use self as the variable to pass into argument 1 of the __init__ function.
+
+All variables in classes will be self.[name of variable] and then will have a comment specifing data type.
 
 Strings must use double quote marks and charaecters by them selves use single.
 
 All projects must use a main.py file.
-    
+
+
+------Basics reporting issues, milestones and pushing, pulling and merging-----
+
+If you report a issues please tag it correctly.
+
+Milestones will have due dates, if you can not finish it with in that time, talk to the project manager(s)
+
+Never directly push to master unless given express permision by the project manager, 
+always make a new branch/fork and ask for review and merge.
+
+Allways pull down the latest version before you start editing.
+
 '''
 
