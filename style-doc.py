efbf50597541282ca1a5c80111ff4f816cc1be51
jedijackatack <jedijackattack@gmail.com>
2016-06-24 13:45:13 +0100

    Update style-doc.py

--- style-doc.py
+++ style-doc.py
@@ -5,24 +5,29 @@ Indent size must be 4
     like this
         and this.
         
-        
 All functions must have docstrings
     def BasicTestFunction(x,y):
         """This is the docstring explain the function here if you don't it won't be merged"""
         print(x,y)
         pass
         
-        
 All variables and functions that are not 1 letter tempary vars must start with capital letters and follow the C 
 variable guide. e.g. onetwothree would be OneTwoThree.
     BasicValue = 4
     def TheFunction(x,BasicValue):
-    "multiplies two numbers"
+    """multiplies two numbers"""
         print(x*BasicValue)
         pass
-        
 
-        
+All functions must end in pass, continue, break or return.
+
+Functions should do one task and one task only. If it can be split into 2 tasks split it. They can call other functions.
+
+A classes __init__ method must be the first function in the class file.
+
+Strings must use double quote marks and charaecters by them selves use single.
+
+All projects must use a main.py file.
     
 '''
 
