e95226c9e8952636fa89fcfcc5a4aecceb635e72
rhombus69 <housecouncil@hotmail.co.uk>
2016-06-24 12:15:16 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+print(' Jedi Painting & Decorating Co.')
+
+while True:
+    ### Menu loop ###
+    print(' = = = = = M E N U = = = = = ')
+    print(' 1 - Create new tab ')
+    print(' 2 - Open tab ')
+    print(' 3 - Delete tab ')
+    print(' 4 - Quit ')
+    option = 
