2f4d589b05d77d7a722959724e0125d54e4a58bf
jedijackatack <jedijackattack@gmail.com>
2016-06-24 12:22:47 +0100

    The style doc is create
    Must read

---
+++ style-doc.py
@@ -0,0 +1,28 @@
+'''
+------Basics-----
+
+Indent size must be 4
+    like this
+        and this.
+        
+        
+All functions must have docstrings
+    def BasicTestFunction(x,y):
+        """This is the docstring explain the function here if you don't it won't be merged"""
+        print(x,y)
+        pass
+        
+        
+All variables and functions that are not 1 letter tempary vars must start with capital letters and follow the C 
+variable guide. e.g. onetwothree would be OneTwoThree.
+    BasicValue = 4
+    def TheFunction(x,BasicValue):
+    "multiplies two numbers"
+        print(x*BasicValue)
+        pass
+        
+
+        
+    
+'''
+
